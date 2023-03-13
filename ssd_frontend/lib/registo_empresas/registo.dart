3d5337538274cc341e9dd2b6ad6dd63fe25a5b77
Utilizador <marianabarbara@ua.pt>
2023-03-13 19:24:33 +0000

    Distritos e Concelhos - Registo Empresa

--- ssd_frontend/lib/registo_empresas/registo.dart
+++ ssd_frontend/lib/registo_empresas/registo.dart
@@ -8,43 +8,357 @@ class RegistoEmpresaPage extends StatefulWidget {
 class _RegistoEmpresaPageState extends State<RegistoEmpresaPage> {
   final _formKey = GlobalKey<FormState>();
 
-  final List<String> _regioes = [
-    'Região Norte',
-    'Região Centro',
-    'Região Sul',
+  final List<String> _distritos = [
+    'Aveiro',
+    'Beja',
+    'Braga',
+    'Bragança',
+    'Castelo Branco',
+    'Coimbra',
+    'Évora',
+    'Faro',
+    'Guarda',
+    'Leiria',
+    'Lisboa',
+    'Portalegre',
+    'Porto',
+    'Santarém',
+    'Setúbal',
+    'Viana do Castelo',
+    'Vila Real',
+    'Viseu',
     'Açores',
     'Madeira'
   ];
 
-  final Map<String, List<String>> _cidadesPorRegiao = {
-    'Região Norte': ['Braga', 'Porto', 'Viana do Castelo', 'Vila Real'],
-    'Região Centro': [
-      'Aveiro',
-      'Castelo Branco',
-      'Coimbra',
-      'Guarda',
-      'Leiria',
-      'Santarém',
-      'Viseu'
+  final Map<String, List<String>> _concelhosPorDistrito = {
+    'Aveiro': [ "Águeda",
+                "Albergaria-a-Velha",
+                "Anadia",
+                "Arouca",
+                "Aveiro",
+                "Castelo de Paiva",
+                "Espinho",
+                "Estarreja",
+                "Ílhavo",
+                "Mealhada",
+                "Murtosa",
+                "Oliveira de Azeméis",
+                "Oliveira do Bairro",
+                "Ovar",
+                "Santa Maria da Feira",
+                "São João da Madeira",
+                "Sever do Vouga",
+                "Vagos",
+                "Vale de Cambra"],
+
+    'Beja': [   "Aljustrel",
+                "Almodôvar",
+                "Alvito",
+                "Barrancos",
+                "Beja",
+                "Castro Verde",
+                "Cuba",
+                "Ferreira do Alentejo",
+                "Mértola",
+                "Moura",
+                "Odemira",
+                "Ourique",
+                "Serpa",
+                "Vidigueira"],
+
+    'Braga': [  "Amares",
+                "Barcelos",
+                "Braga",
+                "Cabeceiras de Basto",
+                "Celorico de Basto",
+                "Esposende",
+                "Fafe",
+                "Guimarães",
+                "Póvoa de Lanhoso",
+                "Terras de Bouro",
+                "Vieira do Minho",
+                "Vila Nova de Famalicão",
+                "Vila Verde",
+                "Vizela"],
+
+    'Bragança':[  "Alfândega da Fé",
+                  "Bragança",
+                  "Carrazeda de Ansiães",
+                  "Freixo de Espada à Cinta",
+                  "Macedo de Cavaleiros",
+                  "Miranda do Douro",
+                  "Mirandela",
+                  "Mogadouro",
+                  "Torre de Moncorvo",
+                  "Vila Flor",
+                  "Vimioso",
+                  "Vinhais"],
+
+    'Castelo Branco': [ "Belmonte",
+                        "Castelo Branco",
+                        "Covilhã",
+                        "Fundão",
+                        "Idanha-a-Nova",
+                        "Oleiros",
+                        "Penamacor",
+                        "Proença-a-Nova",
+                        "Sertã",
+                        "Vila de Rei",
+                        "Vila Velha de Ródão"],
+
+    'Coimbra':[ "Arganil",
+                "Cantanhede",
+                "Coimbra",
+                "Condeixa-a-Nova",
+                "Figueira da Foz",
+                "Góis",
+                "Lousã",
+                "Mealhada",
+                "Mira",
+                "Miranda do Corvo",
+                "Montemor-o-Velho",
+                "Oliveira do Hospital",
+                "Pampilhosa da Serra",
+                "Penacova",
+                "Penela",
+                "Soure",
+                "Tábua",
+                "Vila Nova de Poiares"],
+
+    'Évora': [  "Alandroal",
+                "Arraiolos",
+                "Borba",
+                "Estremoz",
+                "Évora",
+                "Montemor-o-Novo",
+                "Mora",
+                "Mourão",
+                "Portel",
+                "Redondo",
+                "Reguengos de Monsaraz",
+                "Vendas Novas",
+                "Viana do Alentejo",
+                "Vila Viçosa"],
+
+    'Faro': [ "Albufeira",
+              "Alcoutim",
+              "Aljezur",
+              "Castro Marim",
+              "Faro",
+              "Lagoa",
+              "Lagos",
+              "Loulé",
+              "Monchique",
+              "Olhão",
+              "Portimão",
+              "São Brás de Alportel",
+              "Silves",
+              "Tavira",
+              "Vila do Bispo",
+              "Vila Real de Santo António"],
+
+    'Guarda': [ "Aguiar da Beira",
+                "Almeida",
+                "Celorico da Beira",
+                "Figueira de Castelo Rodrigo",
+                "Fornos de Algodres",
+                "Gouveia",
+                "Guarda",
+                "Manteigas",
+                "Mêda",
+                "Pinhel",
+                "Sabugal",
+                "Seia",
+                "Trancoso",
+                "Vila Nova de Foz Côa"],
+
+    'Leiria': [ "Alcobaça",
+                "Alvaiázere",
+                "Ansião",
+                "Batalha",
+                "Bombarral",
+                "Caldas da Rainha",
+                "Castanheira de Pera",
+                "Figueiró dos Vinhos",
+                "Leiria",
+                "Marinha Grande",
+                "Nazaré",
+                "Óbidos",
+                "Pedrógão Grande",
+                "Peniche",
+                "Pombal",
+                "Porto de Mós"],
+
+    'Lisboa': [ "Alenquer",
+                "Amadora",
+                "Arruda dos Vinhos",
+                "Azambuja",
+                "Cadaval",
+                "Cascais",
+                "Lisboa",
+                "Loures",
+                "Lourinhã",
+                "Mafra",
+                "Odivelas",
+                "Oeiras",
+                "Sintra",
+                "Sobral de Monte Agraço",
+                "Torres Vedras",
+                "Vila Franca de Xira"],
+
+    'Portalegre': [ "Alter do Chão",
+                    "Arronches",
+                    "Avis",
+                    "Campo Maior",
+                    "Castelo de Vide",
+                    "Crato",
+                    "Elvas",
+                    "Fronteira",
+                    "Gavião",
+                    "Marvão",
+                    "Monforte",
+                    "Nisa",
+                    "Ponte de Sor",
+                    "Portalegre",
+                    "Sousel" ],
+    'Porto': [  "Amarante",
+                "Baião",
+                "Felgueiras",
+                "Gondomar",
+                "Lousada",
+                "Maia",
+                "Marco de Canaveses",
+                "Matosinhos",
+                "Paços de Ferreira",
+                "Paredes",
+                "Penafiel",
+                "Porto",
+                "Póvoa de Varzim",
+                "Santo Tirso",
+                "Valongo",
+                "Vila do Conde",
+                "Vila Nova de Gaia"
+
     ],
-    'Região Sul': [
-      'Beja',
-      'Faro',
-      'Lisboa',
-      'Portalegre',
-      'Setúbal',
-      'Évora'
-    ],
-    'Açores': [
-      'Angra do Heroísmo',
-      'Horta',
-      'Ponta Delgada',
-      'Santa Cruz da Graciosa',
-      'Santa Cruz das Flores',
-      'São Roque do Pico',
-      'Velas'
-    ],
-    'Madeira': ['Funchal', 'Machico', 'Ponta do Sol', 'Porto Moniz']
+    'Santarém': [ "Abrantes",
+                  "Alcanena",
+                  "Almeirim",
+                  "Alpiarça",
+                  "Benavente",
+                  "Cartaxo",
+                  "Chamusca",
+                  "Constância",
+                  "Coruche",
+                  "Entroncamento",
+                  "Ferreira do Zêzere",
+                  "Golegã",
+                  "Mação",
+                  "Rio Maior",
+                  "Salvaterra de Magos",
+                  "Santarém",
+                  "Sardoal",
+                  "Tomar",
+                  "Torres Novas",
+                  "Vila Nova da Barquinha" ],
+
+    'Setúbal': [  "Alcácer do Sal",
+                  "Alcochete",
+                  "Almada",
+                  "Barreiro",
+                  "Grândola",
+                  "Moita",
+                  "Montijo",
+                  "Palmela",
+                  "Santiago do Cacém",
+                  "Seixal",
+                  "Sesimbra",
+                  "Setúbal",
+                  "Sines" ],
+
+    'Viana do Castelo': [ "Arcos de Valdevez",
+                          "Caminha",
+                          "Melgaço",
+                          "Monção",
+                          "Paredes de Coura",
+                          "Ponte da Barca",
+                          "Ponte de Lima",
+                          "Valença",
+                          "Viana do Castelo",
+                          "Vila Nova de Cerveira" ],
+
+    'Vila Real': [  "Alijó",
+                    "Boticas",
+                    "Chaves",
+                    "Mesão Frio",
+                    "Mondim de Basto",
+                    "Montalegre",
+                    "Murça",
+                    "Peso da Régua",
+                    "Ribeira de Pena",
+                    "Sabrosa",
+                    "Santa Marta de Penaguião",
+                    "Valpaços",
+                    "Vila Pouca de Aguiar",
+                    "Vila Real"],
+
+    'Viseu': ["Armamar",
+              "Carregal do Sal",
+              "Castro Daire",
+              "Cinfães",
+              "Lamego",
+              "Mangualde",
+              "Moimenta da Beira",
+              "Mortágua",
+              "Nelas",
+              "Oliveira de Frades",
+              "Penalva do Castelo",
+              "Penedono",
+              "Resende",
+              "Santa Comba Dão",
+              "São João da Pesqueira",
+              "São Pedro do Sul",
+              "Sátão",
+              "Sernancelhe",
+              "Tabuaço",
+              "Tarouca",
+              "Tondela",
+              "Vila Nova de Paiva",
+              "Viseu",
+              "Vouzela"],
+
+    'Açores': [ "Angra do Heroísmo",
+                "Calheta (São Jorge)",
+                "Corvo",
+                "Horta",
+                "Lagoa (São Miguel)",
+                "Lajes das Flores",
+                "Lajes do Pico",
+                "Madalena",
+                "Nordeste",
+                "Ponta Delgada",
+                "Povoação",
+                "Praia da Vitória",
+                "Ribeira Grande",
+                "Santa Cruz da Graciosa",
+                "Santa Cruz das Flores",
+                "São Roque do Pico",
+                "Velas",
+                "Vila do Corvo",
+                "Vila Franca do Campo",
+                "Vila do Porto" ],
+
+    'Madeira': ["Calheta (Madeira)",
+                "Câmara de Lobos",
+                "Funchal",
+                "Machico",
+                "Ponta do Sol",
+                "Porto Moniz",
+                "Porto Santo",
+                "Ribeira Brava",
+                "Santa Cruz",
+                "Santana",
+                "São Vicente" ]
   };
 
   var _empresa = Empresa();
@@ -135,33 +449,33 @@ class _RegistoEmpresaPageState extends State<RegistoEmpresaPage> {
                 Text('Região da Atividade'),
 
                 Column(
-                  children: _regioes
+                  children: _distritos
                       .map(
-                        (regiao) => Row(
+                        (distrito) => Row(
                           children: <Widget>[
                             Checkbox(
-                              value: _empresa.regioes.contains(regiao),
+                              value: _empresa.distritos.contains(distrito),
                               onChanged: (value) {
                                 setState(() {
                                   if (value!) {
-                                    _empresa.regioes.add(regiao);
+                                    _empresa.distritos.add(distrito);
                                   } else {
-                                    _empresa.regioes.remove(regiao);
+                                    _empresa.distritos.remove(distrito);
                                   }
                                 });
                               },
                             ),
-                            Text(regiao),
-                            if (_empresa.regioes.contains(regiao))
+                            Text(distrito),
+                            if (_empresa.distritos.contains(distrito))
                               Expanded(
                                 child: DropdownButton<String>(
-                                  value: _empresa.cidades[regiao],
+                                  value: _empresa.concelhos[distrito],
                                   onChanged: (String? value) {
                                     setState(() {
-                                      _empresa.cidades[regiao] = value!;
+                                      _empresa.concelhos[distrito] = value!;
                                     });
                                   },
-                                  items: _cidadesPorRegiao[regiao]
+                                  items: _concelhosPorDistrito[distrito]
                                       ?.map(
                                         (cidade) => DropdownMenuItem<String>(
                                           value: cidade,
@@ -212,15 +526,15 @@ class Empresa {
   late String cae;
   late String contacto;
   late String email;
-  List<String> regioes = [];
-  Map<String, String> cidades = {};
+  List<String> distritos = [];
+  Map<String, String> concelhos = {};
   late String website;
 
   @override
   String toString() {
     return 'Empresa(nome: $nome, morada: $morada, nif: $nif, cae: $cae, '
-        'contacto: $contacto, email: $email, regioes: $regioes, '
-        'cidades: $cidades, website: $website)';
+        'contacto: $contacto, email: $email, distritos: $distritos, '
+        'concelhos: $concelhos, website: $website)';
   }
 }
 
