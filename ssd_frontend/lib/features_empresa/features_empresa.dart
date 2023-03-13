cf7985229c2f9d9acab9af049d97c8d6e457d8be
LaraMatos <lara.catarina.matos@ua.pt>
2023-03-13 17:20:28 +0000

    features empresa added

---
+++ ssd_frontend/lib/features_empresa/features_empresa.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class FeaturesEmpresa extends StatefulWidget {
+  const FeaturesEmpresa({Key? key}) : super(key: key);
+
+  @override
+  _FeaturesEmpresaState createState() => _FeaturesEmpresaState();
+}
+
+class _FeaturesEmpresaState extends State<FeaturesEmpresa> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+
+
+}
+
