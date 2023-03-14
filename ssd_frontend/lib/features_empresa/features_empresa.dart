37e55495ebcd56fcc7232c334a2a50b8ddffeeff
LaraMatos <lara.catarina.matos@ua.pt>
2023-03-14 15:49:42 +0000

    mais um commit antes que perca tudo -> update serviços disponiveis

--- ssd_frontend/lib/features_empresa/features_empresa.dart
+++ ssd_frontend/lib/features_empresa/features_empresa.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:ssd_frontend/main.dart';
+import 'package:ssd_frontend/noticias/feature_noticias.dart';
 import 'package:ssd_frontend/servicos/servicos.dart';
 
 class FeaturesEmpresa extends StatefulWidget {
@@ -14,7 +16,7 @@ class _FeaturesEmpresaState extends State<FeaturesEmpresa> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.deepPurpleAccent,
+        backgroundColor: Colors.blue,
         foregroundColor: Colors.white,
         shadowColor: Colors.transparent,
         title: const Text(
@@ -40,6 +42,40 @@ class _FeaturesEmpresaState extends State<FeaturesEmpresa> {
 
                   ListTile(
                     leading: const Icon(
+                        Icons.home
+                    ),
+                    title: const Text(
+                      "Voltar para a Página Principal",
+                      style: TextStyle(
+                        fontSize: 18,
+                      ),
+                    ),
+                    onTap: () {
+                      Navigator.push(context, MaterialPageRoute(
+                          builder: (context) => Destinos())
+                      );
+                    },
+                  ),
+
+                  ListTile(
+                    leading: const Icon(
+                        Icons.home
+                    ),
+                    title: const Text(
+                      "Notícias",
+                      style: TextStyle(
+                        fontSize: 18,
+                      ),
+                    ),
+                    onTap: () {
+                      Navigator.push(context, MaterialPageRoute(
+                          builder: (context) => FeatureNoticias())
+                      );
+                    },
+                  ),
+
+                  ListTile(
+                    leading: const Icon(
                       Icons.dataset_rounded
                     ),
                     title: const Text(
@@ -59,35 +95,188 @@ class _FeaturesEmpresaState extends State<FeaturesEmpresa> {
               ),
             ),
 
-            Container(
-              child: Align(
-                alignment: FractionalOffset.bottomCenter,
-                child: Container(
-                  child: Column(
-                    children: [
-                      Divider(),
-                      ListTile(
-                        leading: const Icon(
-                          Icons.logout,
-                        ),
-                        title: const Text(
-                          "Terminar Sessão",
-                          style: TextStyle(
-                            fontSize: 18
-                          ),
+            Align(
+              alignment: FractionalOffset.bottomCenter,
+              child: Container(
+                child: Column(
+                  children: const [
+                    Divider(),
+                    ListTile(
+                      leading: Icon(
+                        Icons.logout,
+                      ),
+                      title: Text(
+                        "Terminar Sessão",
+                        style: TextStyle(
+                          fontSize: 18
                         ),
                       ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
               ),
             ),
           ],
         ),
       ),
+
+      body: Column(
+        children: [
+          const Expanded(flex: 2, child: _TopPortion()),
+          Expanded(
+            flex: 3,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                children: const [
+                  Text(
+                    "Restaurante 5 maravilhas",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 25,
+                    ),
+                  ),
+
+                  SizedBox(height: 16,),
+
+                  Text(
+                    "5maravilhas@gmail.com",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18,
+                    ),
+                  ),
+
+                  SizedBox(height: 10,),
+
+                  Text(
+                    "232854123",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18,
+                    ),
+                  ),
+
+                  SizedBox(height: 16,),
+
+                  _ProfileInfoRow()
+
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+
+    );
+  }
+}
+
+class _ProfileInfoRow extends StatelessWidget {
+  const _ProfileInfoRow({Key? key}) : super(key: key);
+
+  final List<ProfileInfoItem> _items = const [
+    ProfileInfoItem("Anúncios", 5),
+    ProfileInfoItem("Visitantes", 40),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 80,
+      constraints: const BoxConstraints(maxWidth: 400),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: _items
+            .map((item) => Expanded(
+            child: Row(
+              children: [
+                if (_items.indexOf(item) != 0) const VerticalDivider(),
+                Expanded(child: _singleItem(context, item)),
+              ],
+            )))
+            .toList(),
+      ),
     );
   }
 
+  Widget _singleItem(BuildContext context, ProfileInfoItem item) => Column(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: [
+      Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Text(
+          item.value.toString(),
+          style: const TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 20,
+          ),
+        ),
+      ),
+      Text(
+        item.title,
+        style: Theme.of(context).textTheme.caption,
+      )
+    ],
+  );
 
+}
+
+
+
+class ProfileInfoItem {
+  final String title;
+  final int value;
+  const ProfileInfoItem(this.title, this.value);
+}
+
+
+
+class _TopPortion extends StatelessWidget {
+  const _TopPortion({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      fit: StackFit.expand,
+      children: [
+        Container(
+          margin: const EdgeInsets.only(bottom: 50),
+          decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                  begin: Alignment.bottomCenter,
+                  end: Alignment.topCenter,
+                  colors: [Colors.blueAccent, Colors.blue]),
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(50),
+                bottomRight: Radius.circular(50),
+              )),
+        ),
+        Align(
+          alignment: Alignment.bottomCenter,
+          child: SizedBox(
+            width: 150,
+            height: 150,
+            child: Stack(
+              fit: StackFit.expand,
+              children: [
+                Container(
+                  decoration: const BoxDecoration(
+                    color: Colors.black,
+                    shape: BoxShape.circle,
+                    image: DecorationImage(
+                        fit: BoxFit.cover,
+                        image: AssetImage("assets/json_profiles/profile1.png")
+                    ),
+                  ),
+                ),
+
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
 }
 
--- ssd_frontend/lib/servicos/servicos_disponiveis.dart
+++ ssd_frontend/lib/servicos/servicos_disponiveis.dart
@@ -1,38 +1,123 @@
 import 'package:flutter/material.dart';
 import 'package:ssd_frontend/servicos/cartao_servico.dart';
 import '../componentes/section_title.dart';
+import '../features_empresa/features_empresa.dart';
+import '../main.dart';
 import '../modelos/exemplos_servicos.dart';
+import '../noticias/feature_noticias.dart';
 
 class ServicosDisponiveis extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
       child: Scaffold(
-        body: Container(
-          constraints: BoxConstraints(maxWidth: 1110),
+        appBar: AppBar(
+          backgroundColor: Colors.blue,
+          foregroundColor: Colors.white,
+          shadowColor: Colors.transparent,
+          title: const Text(
+            "Serviços Disponíveis",
+            style: TextStyle(
+                fontSize: 20,
+                color: Colors.white
+            ),
+          ),
+        ),
+
+
+
+        drawer: Drawer(
           child: Column(
             children: [
-              SectionTitle(
-                title: "Serviços Disponíveis",
-                subtitle: "",
-                color: Color(0xFFFF0000),
-                key: ValueKey(1),
-              ),
               Expanded(
-                child: ListView.builder(
-                  itemCount: servicos.length,
-                  itemBuilder: (context, index) {
-                    return Expanded(
-                      child: CartaoServicos(
-                        index: index,
-                        key: ValueKey(index),
+                  child: ListView(
+                    padding: EdgeInsets.zero,
+                    children: [
+                      const DrawerHeader(
+                        child: Image(image: AssetImage("assets/icons/icon_app.png"),
+                        ),
                       ),
-                    );
-                  },
-                ),
+
+                      ListTile(
+                        leading: const Icon(
+                            Icons.home
+                        ),
+                        title: const Text(
+                          "Voltar para a Página Principal",
+                          style: TextStyle(
+                            fontSize: 18,
+                          ),
+                        ),
+                        onTap: () {
+                          Navigator.push(context, MaterialPageRoute(
+                              builder: (context) => Destinos())
+                          );
+                        },
+                      ),
+
+                      ListTile(
+                        leading: const Icon(
+                            Icons.home
+                        ),
+                        title: const Text(
+                          "Área da Empresa",
+                          style: TextStyle(
+                            fontSize: 18,
+                          ),
+                        ),
+                        onTap: () {
+                          Navigator.push(context, MaterialPageRoute(
+                              builder: (context) => FeaturesEmpresa())
+                          );
+                        },
+                      ),
+
+                      ListTile(
+                        leading: const Icon(
+                            Icons.home
+                        ),
+                        title: const Text(
+                          "Notícias",
+                          style: TextStyle(
+                            fontSize: 18,
+                          ),
+                        ),
+                        onTap: () {
+                          Navigator.push(context, MaterialPageRoute(
+                              builder: (context) => FeatureNoticias())
+                          );
+                        },
+                      ),
+
+                    ],
+                  ),
               ),
             ],
           ),
+
+        ),
+
+
+
+        body: Container(
+          constraints: BoxConstraints(maxWidth: 2000),
+            child: Column(
+              children: [
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: servicos.length,
+                    itemBuilder: (context, index) {
+                      return Expanded(
+                        child: CartaoServicos(
+                          index: index,
+                          key: ValueKey(index),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ],
+            ),
         ),
       ),
     );
