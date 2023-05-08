83d6759233f043c64dd3d207295ad4b46946ce1b
JoaoRFigueiredo <jrcidra@ua.pt>
2023-05-08 18:28:18 +0100

    freguesia and concelho support

--- Backend/api/views.py
+++ Backend/api/views.py
@@ -8,6 +8,9 @@ from pymongo import MongoClient
 
 # Create your views here.
 
+##############
+###distrito###
+##############
 @csrf_exempt
 def services_by_distrito_tipo(request, distrito, tipo=None):
     if request.method == 'GET':
@@ -73,84 +76,140 @@ def services_by_distrito(request, distrito):
         return JsonResponse(response_data)
     return JsonResponse({'error': 'Method Not Allowed'}, status=405)
 
+##############
+###Concelho###
+##############
 @csrf_exempt
-def services_by_concelho(request):
+def services_by_concelho_tipo(request, concelho, tipo=None):
     if request.method == 'GET':
-        concelho = request.GET.get('concelho')
+        # distrito = request.GET.get('distrito')
+        # tipo = request.GET.get('tipo')
         client = MongoClient('mongodb://localhost:27017/')
         db = client['mydatabase']
         services = db['Servicos']
         services_to_return = []
+        
+        query = {'concelho': concelho}
+        if tipo:
+            query['tipo_servico'] = tipo
 
-        #anuncio = services.find_one
-
-        # for x in services.find():
-        #     print(x)
-        result = services.find({'concelho': concelho})
+        result = services.find(query)
         result_list = list(result)
         for x in result_list:
             x['_id'] = str(x['_id'])
             services_to_return.append(x)
-            
         
-        # for x in services_to_return:
-        #     print(x)
-
-        # for services_to_return in services_to_return:
-        #         services_to_return['_id'] = str(services_to_return['_id'])
-
+        print(len(services_to_return))
         if len(services_to_return) == 0:
             response_data = {
                 'Error': 'No services found'
             }
         else:
             response_data = {
-                    'concelho_services': services_to_return
-                            }
-            
-
-
+                'district_services': services_to_return
+            }
 
         return JsonResponse(response_data)
     return JsonResponse({'error': 'Method Not Allowed'}, status=405)
 
-
 @csrf_exempt
-def services_by_freguesia(request):
+def services_by_concelho(request, concelho):
     if request.method == 'GET':
-        freguesia = request.GET.get('freguesia')
+        # distrito = request.GET.get('distrito')
+        # tipo = request.GET.get('tipo')
         client = MongoClient('mongodb://localhost:27017/')
         db = client['mydatabase']
         services = db['Servicos']
         services_to_return = []
+        
+        query = {'concelho': concelho}
+        
 
-        #anuncio = services.find_one
-
-        # for x in services.find():
-        #     print(x)
-        result = services.find({'freguesia': freguesia})
+        result = services.find(query)
         result_list = list(result)
         for x in result_list:
             x['_id'] = str(x['_id'])
             services_to_return.append(x)
-            
         
-        # for x in services_to_return:
-        #     print(x)
-
-        # for services_to_return in services_to_return:
-        #         services_to_return['_id'] = str(services_to_return['_id'])
-
+        print(len(services_to_return))
         if len(services_to_return) == 0:
             response_data = {
                 'Error': 'No services found'
             }
         else:
             response_data = {
-                    'freguesia_services': services_to_return
-                }
-            
+                'district_services': services_to_return
+            }
 
         return JsonResponse(response_data)
     return JsonResponse({'error': 'Method Not Allowed'}, status=405)
-    
+
+
+##############
+###Freguesia###
+##############
+@csrf_exempt
+def services_by_freguesia_tipo(request, freguesia, tipo=None):
+    if request.method == 'GET':
+        # distrito = request.GET.get('distrito')
+        # tipo = request.GET.get('tipo')
+        client = MongoClient('mongodb://localhost:27017/')
+        db = client['mydatabase']
+        services = db['Servicos']
+        services_to_return = []
+        
+        query = {'freguesia': freguesia}
+        if tipo:
+            query['tipo_servico'] = tipo
+
+        result = services.find(query)
+        result_list = list(result)
+        for x in result_list:
+            x['_id'] = str(x['_id'])
+            services_to_return.append(x)
+        
+        print(len(services_to_return))
+        if len(services_to_return) == 0:
+            response_data = {
+                'Error': 'No services found'
+            }
+        else:
+            response_data = {
+                'district_services': services_to_return
+            }
+
+        return JsonResponse(response_data)
+    return JsonResponse({'error': 'Method Not Allowed'}, status=405)
+
+@csrf_exempt
+def services_by_freguesia(request, freguesia):
+    if request.method == 'GET':
+        # distrito = request.GET.get('distrito')
+        # tipo = request.GET.get('tipo')
+        client = MongoClient('mongodb://localhost:27017/')
+        db = client['mydatabase']
+        services = db['Servicos']
+        services_to_return = []
+        
+        query = {'freguesia': freguesia}
+        
+
+        result = services.find(query)
+        result_list = list(result)
+        for x in result_list:
+            x['_id'] = str(x['_id'])
+            services_to_return.append(x)
+        
+        print(len(services_to_return))
+        if len(services_to_return) == 0:
+            response_data = {
+                'Error': 'No services found'
+            }
+        else:
+            response_data = {
+                'district_services': services_to_return
+            }
+
+        return JsonResponse(response_data)
+    return JsonResponse({'error': 'Method Not Allowed'}, status=405)
+
